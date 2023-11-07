3b211a1a66369509b88551defa8d6b03a20e7389
Tyler Downing <tyler.ar.downing@hotmail.com>
2023-11-07 15:39:41 -0400

    completed the Etch a sketch project

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-function buildGrid(x) {
-    for(i=0; i<x; i++){
-        for(y=0; y<x; y++){
-            const makeDiv = document.querySelector('#container');
-            const newDiv = document.createElement('div');
-            newDiv.classList.add('box');
-            makeDiv.appendChild(newDiv);
-        }
+let color = "black";
+
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove()); 
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let amount = size * size;
+    for (i=0; i<=amount; i++){
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
     }
 }
 
-buildGrid(16);
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 || input <= 100){
+        populateBoard(input);
+    } else {
+       console.log('To many squares'); 
+    }
+}
+
+function colorSquare () {
+    if(color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}
